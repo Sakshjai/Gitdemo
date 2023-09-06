9f5b8dcf0b0db183d4b537d200532cb3b258fd3d
sakshi <sakshi.b.jain@capgemini.com>
2023-09-06 13:08:23 +0530

    Asia Guy

--- src/test/java/FirstSelenium/OpenClarity.java
+++ src/test/java/FirstSelenium/OpenClarity.java
@@ -4,6 +4,7 @@ public class OpenClarity {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "\"C:\\Users\\sakshjai\\Desktop\\Drivers\\chromedriver-win64\\chromedriver.exe\"");
+		System.out.println ("Java1");
 		WebDriver driver = new ChromeDriver ();
 		
 	}
